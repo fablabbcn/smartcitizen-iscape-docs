4bc29968f6cabee18e156474c3abe3542c6f2727
Viktor Smari <viktorsmari@gmail.com>
2018-08-22 17:03:19 +0200

    Fix filename

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -4,8 +4,8 @@ set -e
 # setup ssh-agent and provide the GitHub deploy key
 eval "$(ssh-agent -s)"
 #openssl aes-256-cbc -K $encrypted_fb17a912150b_key -iv $encrypted_fb17a912150b_iv -in ed25519.enc -out ed25519 -d
-chmod 600 ed25519
-ssh-add ed25519
+chmod 600 deploy-key
+ssh-add deploy-key
 
 # commit the assets in build/ to the gh-pages branch and push to GitHub using SSH
 ./node_modules/.bin/gh-pages -d dist/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
